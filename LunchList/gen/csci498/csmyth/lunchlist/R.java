8241e608e9c3b112401326d72dfe703831d50669
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-09-03 13:42:11 -0600

    Added address TextView to vertical LinearLayout previously created.

--- LunchList/gen/csci498/csmyth/lunchlist/R.java
+++ LunchList/gen/csci498/csmyth/lunchlist/R.java
@@ -19,10 +19,11 @@ public final class R {
     }
     public static final class id {
         public static final int addr=0x7f070002;
+        public static final int address=0x7f07000b;
         public static final int delivery=0x7f070006;
         public static final int details=0x7f070000;
         public static final int icon=0x7f070009;
-        public static final int menu_settings=0x7f07000b;
+        public static final int menu_settings=0x7f07000c;
         public static final int name=0x7f070001;
         public static final int restaurant_spinner=0x7f070008;
         public static final int save=0x7f070007;
