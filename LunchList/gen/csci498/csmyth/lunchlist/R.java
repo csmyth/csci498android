f333cefbe71b52ed7c707fa8a4cf2f89c650238f
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-09-03 04:37:20 -0600

    Corrections made to eliminate warnings: name_tag, addr_tag, and save_button_title added to strings.xml per Eclipse warning suggestion.

--- LunchList/gen/csci498/csmyth/lunchlist/R.java
+++ LunchList/gen/csci498/csmyth/lunchlist/R.java
@@ -31,10 +31,13 @@ public final class R {
         public static final int activity_lunch_list=0x7f060000;
     }
     public static final class string {
+        public static final int addr_tag=0x7f040002;
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_lunch_list=0x7f040003;
+        public static final int menu_settings=0x7f040003;
+        public static final int name_tag=0x7f040005;
+        public static final int save_button_title=0x7f040006;
+        public static final int title_activity_lunch_list=0x7f040004;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
