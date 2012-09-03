620186e7319dd5dabb37f805462b00c9701613b9
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-09-03 02:14:19 -0600

    Stylistic changes all involving whitespace and readability corrections

--- HelloAndroid/src/csci498/hello/HelloActivity.java
+++ HelloAndroid/src/csci498/hello/HelloActivity.java
@@ -7,13 +7,14 @@ import android.widget.Button;
 import java.util.Date;
 
 public class HelloActivity extends Activity implements View.OnClickListener {
+	
 	Button btn;
 
 	@Override
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
 
-		btn=new Button(this);
+		btn = new Button(this);
 		btn.setOnClickListener(this);
 		updateTime();
 		setContentView(btn);
@@ -22,10 +23,10 @@ public class HelloActivity extends Activity implements View.OnClickListener {
 
 	public void onClick(View view) {
 		updateTime();
-
 	}
 
 	private void updateTime() {
 		btn.setText(new Date().toString());
 	}
+	
 }
