ac317fd0aaf83e53c2febc03eafaebfe8e02e57b
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-09-03 06:21:53 -0600

    Updated LunchList.java to use ArrayList of Restaurants instead of just one.

--- LunchList/src/csci498/csmyth/lunchlist/LunchList.java
+++ LunchList/src/csci498/csmyth/lunchlist/LunchList.java
@@ -1,5 +1,8 @@
 package csci498.csmyth.lunchlist;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
@@ -10,7 +13,7 @@ import android.widget.RadioGroup;
 
 public class LunchList extends Activity {
 	
-	Restaurant r = new Restaurant();
+	List<Restaurant> model = new ArrayList<Restaurant>();
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,7 @@ public class LunchList extends Activity {
     private View.OnClickListener onSave = new View.OnClickListener() {
 		
 		public void onClick(View v) {
+			Restaurant r = new Restaurant();
 			EditText name = (EditText)findViewById(R.id.name);
 			EditText address = (EditText)findViewById(R.id.addr);
 			
