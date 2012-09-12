e507ab0dc073c979c52dee4207be20e2f96124f0
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-09-12 11:09:35 -0600

    Based on work from Tutorial #7, was able to get ScrollView working with details view. DatePicker added to view.

--- LunchList/gen/csci498/csmyth/lunchlist/R.java
+++ LunchList/gen/csci498/csmyth/lunchlist/R.java
@@ -21,17 +21,18 @@ public final class R {
     }
     public static final class id {
         public static final int addr=0x7f070003;
-        public static final int address=0x7f07000b;
+        public static final int address=0x7f07000c;
+        public static final int date=0x7f070008;
         public static final int delivery=0x7f070007;
         public static final int details=0x7f070001;
-        public static final int icon=0x7f070009;
-        public static final int menu_settings=0x7f07000c;
+        public static final int icon=0x7f07000a;
+        public static final int menu_settings=0x7f07000d;
         public static final int name=0x7f070002;
         public static final int restaurants=0x7f070000;
-        public static final int save=0x7f070008;
+        public static final int save=0x7f070009;
         public static final int sit_down=0x7f070006;
         public static final int take_out=0x7f070005;
-        public static final int title=0x7f07000a;
+        public static final int title=0x7f07000b;
         public static final int types=0x7f070004;
     }
     public static final class layout {
