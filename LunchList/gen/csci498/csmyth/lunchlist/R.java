c8965dac67bd55b3c394cb12181c464eb01e9b6d
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-09-12 09:00:22 -0600

    New attempt to divide up the activity_lunch_list.xml file into two xml files in order to get ScrollView to work for the details tab. Need this for DatePicker because it takes up so much space. Failure. App crashes every time it is opened.

--- LunchList/gen/csci498/csmyth/lunchlist/R.java
+++ LunchList/gen/csci498/csmyth/lunchlist/R.java
@@ -36,9 +36,10 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_lunch_list=0x7f030000;
-        public static final int row=0x7f030001;
-        public static final int row2=0x7f030002;
-        public static final int row3=0x7f030003;
+        public static final int details_view=0x7f030001;
+        public static final int row=0x7f030002;
+        public static final int row2=0x7f030003;
+        public static final int row3=0x7f030004;
     }
     public static final class menu {
         public static final int activity_lunch_list=0x7f060000;
