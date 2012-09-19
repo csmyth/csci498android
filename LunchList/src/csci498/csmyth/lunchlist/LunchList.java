982bfb56370af799e869246104eec59e5f821f7f
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-09-19 13:10:12 -0600

    Removed extraneous code from LunchList.java: all lines of code involving threads (progress bar work) and all lines involving the options menu.

--- LunchList/src/csci498/csmyth/lunchlist/LunchList.java
+++ LunchList/src/csci498/csmyth/lunchlist/LunchList.java
@@ -35,8 +35,6 @@ public class LunchList extends TabActivity {
 	RadioGroup types = null;
 	EditText notes = null;
 	Restaurant current = null;
-	int progress = 0;
-	AtomicBoolean isActive = new AtomicBoolean(true);
 	
 	static final int ROW_TYPE_TAKE_OUT = 1;
 	static final int ROW_TYPE_SIT_DOWN = 2;
@@ -48,7 +46,6 @@ public class LunchList extends TabActivity {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_PROGRESS);
         setContentView(R.layout.activity_lunch_list);
         
         name = (EditText)findViewById(R.id.name);
@@ -87,52 +84,6 @@ public class LunchList extends TabActivity {
         getTabHost().setCurrentTab(LIST_TAB);
         
     }
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		new MenuInflater(this).inflate(R.menu.option, menu);
-		
-		return(super.onCreateOptionsMenu(menu));
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		if (item.getItemId() == R.id.toast) {
-			String message = "No restaurant selected";
-			
-			if (current != null) {
-				message = current.getNotes();
-			}
-			
-			Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-			
-			return(true);
-		} else if (item.getItemId() == R.id.run) {
-			startWork();
-			
-			return(true);
-		}
-		
-		return(super.onOptionsItemSelected(item));
-	}
-	
-	@Override
-	public void onPause() {
-		super.onPause();
-		
-		isActive.set(false);
-	}
-	
-	@Override
-	public void onResume() {
-		super.onResume();
-		
-		isActive.set(true);
-		
-		if (progress > 0) {
-			startWork();
-		}
-	}
 	
 	private View.OnClickListener onSave = new View.OnClickListener() {
     	public void onClick(View v) {
@@ -266,38 +217,5 @@ public class LunchList extends TabActivity {
 			}
 		}	
 	}
-	
-	private void doSomeLongWork(final int incr) {
-		runOnUiThread(new Runnable() {
-			public void run() {
-				progress += incr;
-				setProgress(progress);
-			}
-		});
-		
-		SystemClock.sleep(250); // Yay, meaningless work!
-	}
-	
-	private void startWork() {
-		setProgressBarVisibility(true);
-		new Thread(longTask).start();
-	}
-	
-	private Runnable longTask = new Runnable() {
-		public void run() {
-			for (int i = progress; i < 10000 && isActive.get(); i += 200) {
-				doSomeLongWork(200);
-			}
-			
-			if (isActive.get()) {
-				runOnUiThread(new Runnable() {
-					public void run() {
-						setProgressBarVisibility(false);
-						progress = 0;
-					}
-				});
-			}
-		}
-	};
 
 }
