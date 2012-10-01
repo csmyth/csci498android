27ff0c9da252fa5e073f76ff425c3f8668e9da22
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-10-01 02:25:49 -0600

    Skeleton code created for new DetailForm class in preparation for splitting the tabs view

---
+++ LunchList/src/csci498/csmyth/lunchlist/DetailForm.java
@@ -0,0 +1,12 @@
+package csci498.csmyth.lunchlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailForm extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// setContentView(R.layout.activity_lunch_list);
+	}
+}
