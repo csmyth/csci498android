d62d5801822eb2006129da498f0fe506e9cd10ff
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-10-11 19:29:50 -0600

    Moved onSave function from LunchList.java to DetailForm.java

--- LunchList/src/csci498/csmyth/lunchlist/DetailForm.java
+++ LunchList/src/csci498/csmyth/lunchlist/DetailForm.java
@@ -2,6 +2,7 @@ package csci498.csmyth.lunchlist;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
@@ -28,4 +29,25 @@ public class DetailForm extends Activity {
         Button save = (Button)findViewById(R.id.save);
         save.setOnClickListener(onSave);
 	}
+	
+	private View.OnClickListener onSave = new View.OnClickListener() {
+    	public void onClick(View v) {
+			String type = null;
+			
+			switch (types.getCheckedRadioButtonId()) {
+			case R.id.sit_down:
+				type = "@string/sit_down";
+				break;
+				
+			case R.id.take_out:
+				type = "@string/take_out";
+				break;
+				
+			case R.id.delivery:
+				type = "@string/delivery";
+				break;
+			}
+		}
+	};
+	
 }
