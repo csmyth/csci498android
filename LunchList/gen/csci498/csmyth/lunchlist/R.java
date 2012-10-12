8309777e7862a028dec3ea9522301411dfd46da5
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-10-12 03:46:11 -0600

    Added preferences.xml, setting up a PreferenceScreen utilizing the previously created string-array resources. Three new entries in strings.xml to accompany.

--- LunchList/gen/csci498/csmyth/lunchlist/R.java
+++ LunchList/gen/csci498/csmyth/lunchlist/R.java
@@ -9,8 +9,8 @@ package csci498.csmyth.lunchlist;
 
 public final class R {
     public static final class array {
-        public static final int sort_clauses=0x7f040001;
-        public static final int sort_names=0x7f040000;
+        public static final int sort_clauses=0x7f050001;
+        public static final int sort_names=0x7f050000;
     }
     public static final class attr {
     }
@@ -27,19 +27,19 @@ public final class R {
         public static final int toast=0x7f020009;
     }
     public static final class id {
-        public static final int add=0x7f08000c;
-        public static final int addr=0x7f080002;
-        public static final int address=0x7f08000b;
-        public static final int delivery=0x7f080006;
-        public static final int details=0x7f080000;
-        public static final int icon=0x7f080009;
-        public static final int name=0x7f080001;
-        public static final int notes=0x7f080007;
-        public static final int save=0x7f080008;
-        public static final int sit_down=0x7f080005;
-        public static final int take_out=0x7f080004;
-        public static final int title=0x7f08000a;
-        public static final int types=0x7f080003;
+        public static final int add=0x7f09000c;
+        public static final int addr=0x7f090002;
+        public static final int address=0x7f09000b;
+        public static final int delivery=0x7f090006;
+        public static final int details=0x7f090000;
+        public static final int icon=0x7f090009;
+        public static final int name=0x7f090001;
+        public static final int notes=0x7f090007;
+        public static final int save=0x7f090008;
+        public static final int sit_down=0x7f090005;
+        public static final int take_out=0x7f090004;
+        public static final int title=0x7f09000a;
+        public static final int types=0x7f090003;
     }
     public static final class layout {
         public static final int detail_form=0x7f030000;
@@ -47,24 +47,30 @@ public final class R {
         public static final int row=0x7f030002;
     }
     public static final class menu {
-        public static final int option=0x7f070000;
+        public static final int option=0x7f080000;
     }
     public static final class string {
-        public static final int addr_tag=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int delivery=0x7f05000a;
-        public static final int hello_world=0x7f050001;
-        public static final int menu_settings=0x7f050003;
-        public static final int name_tag=0x7f050005;
-        public static final int new_restaurant_prompt=0x7f05000c;
-        public static final int notes_tag=0x7f05000b;
-        public static final int save_button_title=0x7f050006;
-        public static final int sit_down=0x7f050008;
-        public static final int take_out=0x7f050009;
-        public static final int title_main=0x7f050004;
-        public static final int type_tag=0x7f050007;
+        public static final int addr_tag=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int delivery=0x7f06000a;
+        public static final int hello_world=0x7f060001;
+        public static final int menu_settings=0x7f060003;
+        public static final int name_tag=0x7f060005;
+        public static final int new_restaurant_prompt=0x7f06000c;
+        public static final int notes_tag=0x7f06000b;
+        public static final int save_button_title=0x7f060006;
+        public static final int sit_down=0x7f060008;
+        public static final int sort_dialog_title=0x7f06000f;
+        public static final int sort_summary=0x7f06000e;
+        public static final int sort_title=0x7f06000d;
+        public static final int take_out=0x7f060009;
+        public static final int title_main=0x7f060004;
+        public static final int type_tag=0x7f060007;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f070000;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
     }
 }
