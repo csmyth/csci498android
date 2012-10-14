ace95a04cc1b2b7377d777a0f7a89684e26a4c30
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-10-14 00:01:15 -0600

    Created override of onRestoreInstanceState in DetailForm.java to repopulate detail form with data stored in the bundle upon being restored (such as after rotation)

--- LunchList/src/csci498/csmyth/lunchlist/DetailForm.java
+++ LunchList/src/csci498/csmyth/lunchlist/DetailForm.java
@@ -56,6 +56,16 @@ public class DetailForm extends Activity {
 		state.putInt("type", types.getCheckedRadioButtonId());
 	}
 	
+	@Override
+	public void onRestoreInstanceState(Bundle state) {
+		super.onRestoreInstanceState(state);
+		
+		name.setText(state.getString("name"));
+		address.setText(state.getString("address"));
+		notes.setText(state.getString("notes"));
+		types.check(state.getInt("type"));
+	}
+	
 	private void load() {
 		Cursor c = helper.getById(restaurantId);
 		
