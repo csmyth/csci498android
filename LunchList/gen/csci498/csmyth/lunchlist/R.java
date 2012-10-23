d5a33376af5b269f23d4e8f841422ecc7be42caa
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-10-23 00:20:06 -0600

    detail_form.xml updated to add feed field. Layout changed slightly to make room. strings.xml updated accordingly

--- LunchList/gen/csci498/csmyth/lunchlist/R.java
+++ LunchList/gen/csci498/csmyth/lunchlist/R.java
@@ -28,19 +28,20 @@ public final class R {
         public static final int toast=0x7f02000a;
     }
     public static final class id {
-        public static final int add=0x7f09000c;
+        public static final int add=0x7f09000d;
         public static final int addr=0x7f090002;
-        public static final int address=0x7f09000b;
+        public static final int address=0x7f09000c;
         public static final int delivery=0x7f090006;
         public static final int details=0x7f090000;
-        public static final int icon=0x7f090009;
+        public static final int feed=0x7f090008;
+        public static final int icon=0x7f09000a;
         public static final int name=0x7f090001;
         public static final int notes=0x7f090007;
-        public static final int prefs=0x7f09000d;
-        public static final int save=0x7f090008;
+        public static final int prefs=0x7f09000e;
+        public static final int save=0x7f090009;
         public static final int sit_down=0x7f090005;
         public static final int take_out=0x7f090004;
-        public static final int title=0x7f09000a;
+        public static final int title=0x7f09000b;
         public static final int types=0x7f090003;
     }
     public static final class layout {
@@ -55,6 +56,7 @@ public final class R {
         public static final int addr_tag=0x7f060002;
         public static final int app_name=0x7f060000;
         public static final int delivery=0x7f06000a;
+        public static final int feed_tag=0x7f060010;
         public static final int hello_world=0x7f060001;
         public static final int menu_settings=0x7f060003;
         public static final int name_tag=0x7f060005;
