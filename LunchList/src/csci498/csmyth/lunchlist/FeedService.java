bebcc31adfa6e00ffa3b2784d422273c0a352355
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-10-28 00:03:45 -0600

    Preliminary version of onHandleIntent in FeedService.java filled in. Static final data members added in association with this implementation

--- LunchList/src/csci498/csmyth/lunchlist/FeedService.java
+++ LunchList/src/csci498/csmyth/lunchlist/FeedService.java
@@ -1,10 +1,18 @@
 package csci498.csmyth.lunchlist;
 
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSReader;
+
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 public class FeedService extends IntentService {
+	private static final String APP_NAME = "LunchList";
+	private static final String PARSE_FEED_EXCEPTION = "Exception parsing feed";
+	
 	public static final String FEED_SERVICE_NAME = "FeedService";
+	public static final String EXTRA_URL = "csci498.csmyth.lunchlist.EXTRA_URL";
 	
 	public FeedService() {
 		super(FEED_SERVICE_NAME);
@@ -12,7 +20,13 @@ public class FeedService extends IntentService {
 	
 	@Override
 	public void onHandleIntent(Intent intent) {
-		// TODO: do something
+		RSSReader reader = new RSSReader();
+		
+		try {
+			RSSFeed result = reader.load(intent.getStringExtra(EXTRA_URL));
+		} catch (Exception excp) {
+			Log.e(APP_NAME, PARSE_FEED_EXCEPTION, excp);
+		}
 	}
 
 }
