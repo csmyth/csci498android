621a77c835a14a6d854b9e2419d47f25c074779d
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-10-27 23:51:35 -0600

    Added stub FeedService.java class to project. New class is as of yet incomplete, to be filled in later

---
+++ LunchList/src/csci498/csmyth/lunchlist/FeedService.java
@@ -0,0 +1,16 @@
+package csci498.csmyth.lunchlist;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class FeedService extends IntentService {
+	public FeedService() {
+		super("FeedService");
+	}
+	
+	@Override
+	public void onHandleIntent(Intent intent) {
+		// TODO: do something
+	}
+
+}
