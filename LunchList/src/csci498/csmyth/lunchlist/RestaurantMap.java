4dd2c17b408f0489d1f021a5f5aa8c6d2337a1a7
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-11-04 02:10:27 -0700

    Added RestaurantMap.java to package. Class not filled in yet.

---
+++ LunchList/src/csci498/csmyth/lunchlist/RestaurantMap.java
@@ -0,0 +1,13 @@
+package csci498.csmyth.lunchlist;
+
+import com.google.android.maps.MapActivity;
+
+public class RestaurantMap extends MapActivity {
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
