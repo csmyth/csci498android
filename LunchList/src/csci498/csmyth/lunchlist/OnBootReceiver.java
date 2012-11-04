7988fa73fc4d3767888b95a1f1acdac4b4900788
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-11-04 04:02:25 -0700

    OnBootReceiver class added to project for restoring alarm schedule on reboot of device. Class currently empty, will be filled in next

---
+++ LunchList/src/csci498/csmyth/lunchlist/OnBootReceiver.java
@@ -0,0 +1,14 @@
+package csci498.csmyth.lunchlist;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnBootReceiver extends BroadcastReceiver {
+	@Override
+	public void onReceive(Context arg0, Intent arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
