6bcf07a033bc253b9e73c47841c602da79ce797b
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-11-04 02:28:23 -0700

    Added public static final data members to RestaurantMap.java to use as Intent extra keys for passing location from DetailForm to RestaurantMap

--- LunchList/src/csci498/csmyth/lunchlist/RestaurantMap.java
+++ LunchList/src/csci498/csmyth/lunchlist/RestaurantMap.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import com.google.android.maps.MapActivity;
 
 public class RestaurantMap extends MapActivity {
+	public static final String EXTRA_LATITUDE = "csci498.csmyth.lunchlist.EXTRA_LATITUDE";
+	public static final String EXTRA_LONGITUDE = "csci498.csmyth.lunchlist.EXTRA_LONGITUDE";
+	public static final String EXTRA_NAME = "csci498.csmyth.lunchlist.EXTRA_NAME";
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
