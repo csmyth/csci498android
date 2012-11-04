ef52b57e3915210e39cdf0156188c6e031f23de7
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-11-04 05:10:34 -0700

    Subtle alarm notification preference added to preferences.xml

--- LunchList/gen/csci498/csmyth/lunchlist/R.java
+++ LunchList/gen/csci498/csmyth/lunchlist/R.java
@@ -62,9 +62,7 @@ public final class R {
     }
     public static final class string {
         public static final int addr_tag=0x7f060002;
-        public static final int alarm_key=0x7f060018;
         public static final int alarm_summary=0x7f060017;
-        public static final int alarm_time_key=0x7f060019;
         public static final int alarm_title=0x7f060016;
         public static final int app_name=0x7f060000;
         public static final int default_time=0x7f060014;
@@ -82,8 +80,10 @@ public final class R {
         public static final int sort_dialog_title=0x7f06000f;
         public static final int sort_summary=0x7f06000e;
         public static final int sort_title=0x7f06000d;
+        public static final int subtle_alarm_summary=0x7f06001a;
+        public static final int subtle_alarm_title=0x7f060019;
         public static final int take_out=0x7f060009;
-        public static final int time_notice=0x7f06001a;
+        public static final int time_notice=0x7f060018;
         public static final int time_summary=0x7f060015;
         public static final int time_title=0x7f060013;
         public static final int title_main=0x7f060004;
