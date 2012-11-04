0f0a99d3549a7fc4c5dd11a1c63307fbcda009c3
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-11-03 19:31:11 -0600

    onHandleIntent() in FeedService.java updated to include messenger for communication with FeedActivity

--- LunchList/src/csci498/csmyth/lunchlist/FeedService.java
+++ LunchList/src/csci498/csmyth/lunchlist/FeedService.java
@@ -3,13 +3,17 @@ package csci498.csmyth.lunchlist;
 import org.mcsoxford.rss.RSSFeed;
 import org.mcsoxford.rss.RSSReader;
 
+import android.app.Activity;
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Message;
+import android.os.Messenger;
 import android.util.Log;
 
 public class FeedService extends IntentService {
 	private static final String APP_NAME = "LunchList";
 	private static final String PARSE_FEED_EXCEPTION = "Exception parsing feed";
+	private static final String SEND_RESULTS_EXCEPTION = "Exception sending results to activity";
 	
 	public static final String FEED_SERVICE_NAME = "FeedService";
 	public static final String EXTRA_URL = "csci498.csmyth.lunchlist.EXTRA_URL";
@@ -21,11 +25,23 @@ public class FeedService extends IntentService {
 	@Override
 	public void onHandleIntent(Intent intent) {
 		RSSReader reader = new RSSReader();
+		Messenger messenger = (Messenger)intent.getExtras().get(EXTRA_MESSENGER);
+		Message msg = Message.obtain();
 		
 		try {
 			RSSFeed result = reader.load(intent.getStringExtra(EXTRA_URL));
+			msg.arg1 = Activity.RESULT_OK;
+			msg.obj = result;
 		} catch (Exception excp) {
 			Log.e(APP_NAME, PARSE_FEED_EXCEPTION, excp);
+			msg.arg1 = Activity.RESULT_CANCELED;
+			msg.obj = excp;
+		}
+		
+		try {
+			messenger.send(msg);
+		} catch (Exception excp) {
+			Log.w(APP_NAME, SEND_RESULTS_EXCEPTION, excp);
 		}
 	}
 
