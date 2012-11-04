a63c19c5744a294a2e9ac8ae06d7bcd59d23f7c2
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-11-04 05:39:44 -0700

    Cleaned up OnBootReceiver.java with static final data members

--- LunchList/src/csci498/csmyth/lunchlist/OnBootReceiver.java
+++ LunchList/src/csci498/csmyth/lunchlist/OnBootReceiver.java
@@ -11,6 +11,14 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
 public class OnBootReceiver extends BroadcastReceiver {
+	private static final String TIME_PREFERENCE_KEY = "alarm_time";
+	private static final String DEFAULT_TIME = "12:00";
+	private static final int SET_SECOND = 0;
+	private static final int SET_MILLISECOND = 0;
+	private static final int INCREMENT = 1;
+	private static final int REQUEST_CODE = 0;
+	private static final int NUM_FLAGS = 0;
+	
 	@Override
 	public void onReceive(Context ctxt, Intent intent) {
 		setAlarm(ctxt);
@@ -20,15 +28,15 @@ public class OnBootReceiver extends BroadcastReceiver {
 		AlarmManager mgr = (AlarmManager)ctxt.getSystemService(Context.ALARM_SERVICE);
 		Calendar cal = Calendar.getInstance();
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctxt);
-		String time = prefs.getString("alarm_time", "12:00");
+		String time = prefs.getString(TIME_PREFERENCE_KEY, DEFAULT_TIME);
 		
 		cal.set(Calendar.HOUR_OF_DAY, TimePreference.getHour(time));
 		cal.set(Calendar.MINUTE, TimePreference.getMinute(time));
-		cal.set(Calendar.SECOND, 0);
-		cal.set(Calendar.MILLISECOND, 0);
+		cal.set(Calendar.SECOND, SET_SECOND);
+		cal.set(Calendar.MILLISECOND, SET_MILLISECOND);
 		
 		if (cal.getTimeInMillis() < System.currentTimeMillis()) {
-			cal.add(Calendar.DAY_OF_YEAR, 1);
+			cal.add(Calendar.DAY_OF_YEAR, INCREMENT);
 		}
 		
 		mgr.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), AlarmManager.INTERVAL_DAY, getPendingIntent(ctxt));	
@@ -41,7 +49,7 @@ public class OnBootReceiver extends BroadcastReceiver {
 	
 	private static PendingIntent getPendingIntent(Context ctxt) {
 		Intent intent = new Intent(ctxt, OnAlarmReceiver.class);
-		return PendingIntent.getBroadcast(ctxt, 0, intent, 0);
+		return PendingIntent.getBroadcast(ctxt, REQUEST_CODE, intent, NUM_FLAGS);
 	}
 
 }
