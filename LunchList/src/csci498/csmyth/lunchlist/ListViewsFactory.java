3098edda9365b9b29d0e6c40d3401cdc20b0442d
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-11-27 13:29:39 -0700

    Removed all @Override statements from ListViewsFactory.java and added @TargetApi in response to error messages. Without these, doesn't match textbook, but runs as it should on device (can add widget, has a listview, can scroll if overfull, goes to detail view when clicked)

--- LunchList/src/csci498/csmyth/lunchlist/ListViewsFactory.java
+++ LunchList/src/csci498/csmyth/lunchlist/ListViewsFactory.java
@@ -1,13 +1,15 @@
 package csci498.csmyth.lunchlist;
 
-import android.R;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.RemoteViewsService;
 import android.widget.RemoteViews;
 
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class ListViewsFactory implements RemoteViewsService.RemoteViewsFactory {
 	private Context ctxt = null;
 	private RestaurantHelper helper = null;
@@ -17,7 +19,6 @@ public class ListViewsFactory implements RemoteViewsService.RemoteViewsFactory {
 		this.ctxt = ctxt;
 	}
 	
-	@Override
 	public void onCreate() {
 		helper = new RestaurantHelper(ctxt);
 		restaurants = helper
@@ -25,18 +26,15 @@ public class ListViewsFactory implements RemoteViewsService.RemoteViewsFactory {
 				.rawQuery("SELECT _ID, name FROM restaurants", null);
 	}
 	
-	@Override
 	public void onDestroy() {
 		restaurants.close();
 		helper.close();
 	}
 	
-	@Override
 	public int getCount() {
 		return restaurants.getCount();
 	}
 	
-	@Override
 	public RemoteViews getViewAt(int position) {
 		RemoteViews row = new RemoteViews(ctxt.getPackageName(), R.layout.widget_row);
 		Intent intent = new Intent();
@@ -50,28 +48,23 @@ public class ListViewsFactory implements RemoteViewsService.RemoteViewsFactory {
 		return row;
 	}
 	
-	@Override
 	public RemoteViews getLoadingView() {
 		return null;
 	}
 	
-	@Override
 	public int getViewTypeCount() {
 		return 1;
 	}
 	
-	@Override
 	public long getItemId(int position) {
 		restaurants.moveToPosition(position);
 		return restaurants.getInt(0);
 	}
 	
-	@Override
 	public boolean hasStableIds() {
 		return true;
 	}
 	
-	@Override
 	public void onDataSetChanged() {
 		// no-op
 	}
