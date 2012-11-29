3a55500a88a3a796c734a882c187a651317c8d62
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-11-28 21:43:58 -0700

    Removed unused imports in LunchList.java

--- LunchList/src/csci498/csmyth/lunchlist/LunchList.java
+++ LunchList/src/csci498/csmyth/lunchlist/LunchList.java
@@ -1,22 +1,8 @@
 package csci498.csmyth.lunchlist;
 
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentActivity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.CursorAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 
 public class LunchList extends FragmentActivity implements LunchFragment.OnRestaurantListener {
 	public final static String ID_EXTRA = "csci498.csmyth.lunchlist._ID";
